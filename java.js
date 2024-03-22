eeec8bfb066de1f69151c7d772579220dd4b1963
¨ibou996¨ <asslevrai02@gmail.com>
2024-03-22 15:02:11 +0000

    first

--- java.js
+++ java.js
@@ -1,36 +1,76 @@
-let ville = "Dakar";
-recevoirTemperature(ville);
+let villeChoisie ;
 
-let btnchangerVille = document.querySelector('#changer');
-
-btnchangerVille.addEventListener('click', () => {
-    ville = prompt("Quelle ville Souhaiter vous voir ?");
-    recevoirTemperature(ville);
-})
-
-
- function recevoirTemperature(ville) {
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=9e18c4a89473275664f2e2a07594b716&units=metric';
-
-let requete = new XMLHttpRequest();
-requete.open('GET', url);
-requete.responseType = 'json';
-requete.send();
-
-requete.onload = function() {
-    if(requete.readyState === XMLHttpRequest.DONE){
-        if(requete.status === 200){
-            let reponse     = requete.response;
-            let temperature = reponse.main.temp;
-            let ville       = reponse.name;
-            document.querySelector('#temperature_label').textContent = temperature;
-            document.querySelector('#ville').textContent             = ville;
+if("geolocation" in navigator) {
+    navigator.geolocation.watchPosition( (position) => {
+        const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+        console.log(url);
+        let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+        requete.open('GET', url); // Nous récupérons juste des données
+        requete.responseType = 'json'; // Nous attendons du JSON
+         requete.send(); // Nous envoyons notre requête
+      
+        // Dès qu'on reçoit une réponse, cette fonction est executée
+        requete.onload = function() {
+          if (requete.readyState === XMLHttpRequest.DONE) {
+            if (requete.status === 200) {
+              let reponse = requete.response;
+              // console.log(reponse);
+              let temperature = reponse.main.temp;
+              let ville       = reponse.name;
+              // console.log(temperature);
+              document.querySelector('#temperature_label').textContent = temperature;
+              document.querySelector('#ville').textContent = ville;
+            }
+            else {
+              alert('Un problème est intervenu, merci de revenir plus tard.');
+            }
+          }
         }
-        else{
-            alert('Un probléme est intervenu, merci de revenir plus tard.')
-        }
-        
-    }
-    
+    }, erreur, options);
 }
+else {
+    villeChoisie = 'Paris';
+    recevoirTemperature(villeChoisie);
+}
+
+var options = {
+    enableHighAccuracy: true
+}
+
+let changerDeVille = document.querySelector('#changer');
+changerDeVille.addEventListener('click', () => {
+  villeChoisie = prompt('Quelle ville souhaitez-vous voir ?');
+  recevoirTemperature(villeChoisie);
+});
+
+function erreur() {
+    villeChoisie = 'Paris';
+    recevoirTemperature(villeChoisie);
+}
+
+function recevoirTemperature(ville) {
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+
+  let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+  requete.open('GET', url); // Nous récupérons juste des données
+  requete.responseType = 'json'; // Nous attendons du JSON
+  requete.send(); // Nous envoyons notre requête
+
+  // Dès qu'on reçoit une réponse, cette fonction est executée
+  requete.onload = function() {
+    if (requete.readyState === XMLHttpRequest.DONE) {
+      if (requete.status === 200) {
+        let reponse = requete.response;
+        // console.log(reponse);
+        let temperature = reponse.main.temp;
+        let ville       = reponse.name;
+        // console.log(temperature);
+        document.querySelector('#temperature_label').textContent = temperature;
+        document.querySelector('#ville').textContent = ville;
+      }
+      else {
+        alert('Un problème est intervenu, merci de revenir plus tard.');
+      }
+    }
+  }
 }
